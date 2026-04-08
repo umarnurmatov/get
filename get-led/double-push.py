1a35bfd54a2f213ddd17ed7124152e1f8b0f8be6
umarnurmatov <umarkhonnur12@gmail.com>
2026-04-08 18:30:29 +0300

    added double-push script

---
+++ get-led/double-push.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+
+leds = [16, 12, 25, 17, 27, 23, 22, 24]
+sleep_time = 0.2
+num = 0
+up = 9
+down = 10
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(leds, GPIO.OUT)
+GPIO.setup(down, GPIO.IN)
+GPIO.setup(up, GPIO.IN)
+GPIO.output(leds, 0)
+
+def dec2bin(value):
+	return [int(element) for element in bin(value)[2:].zfill(8)]
+
+while True:
+	up_st = GPIO.input(up)
+	down_st = GPIO.input(down)
+
+	if up_st and down_st:
+		num = 2**len(leds)-1
+
+	elif up_st:
+		num = min(num+1, 2**len(leds) - 1)
+
+	elif down_st:
+		num = max(num-1, 0)
+
+	print(num, dec2bin(num))
+	time.sleep(sleep_time)
+	GPIO.output(leds, dec2bin(num))
+		
