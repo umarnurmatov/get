37939c0e212ea7d27feba55878734c8a877df73d
umarnurmatov <umarkhonnur12@gmail.com>
2026-04-08 18:24:06 +0300

    added bin-counter script

---
+++ get-led/bin-counter.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+leds = [16, 12, 25, 17, 27, 23, 22, 24]
+sleep_time = 0.2
+num = 0
+up = 9
+down = 10
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(leds, GPIO.OUT)
+GPIO.setup(down, GPIO.IN)
+GPIO.setup(up, GPIO.IN)
+GPIO.output(leds, 0)
+
+def dec2bin(value):
+	return [int(element) for element in bin(value)[2:].zfill(8)]
+
+while True:
+	if GPIO.input(up):
+		num = (num + 1) % 2**len(leds)
+		print(num, dec2bin(num))
+		time.sleep(sleep_time)
+	if GPIO.input(down):
+		num = max(num-1, 0)
+		print(num, dec2bin(num))
+		time.sleep(sleep_time)
+
+	GPIO.output(leds, dec2bin(num))
+		
